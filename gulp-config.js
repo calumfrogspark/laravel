14fdf5780b6b562ddc524a6945233e42e78a90fc
Calum Ogilvie <55081509+calumfrogspark@users.noreply.github.com>
2021-10-14 08:21:33 +0100

    updating composer & npm packages, removing laravel dummy sql script, updating sanmple env file and removing gulp workflow

--- gulp-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  environment: 'development',
-  development () {
-    return this.environment === 'development';
-  },
-  production () {
-    return this.environment === 'production';
-  }
-}
